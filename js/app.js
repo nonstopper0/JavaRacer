473b9d487c17f6023d5835f2d1e5ac0c109d34a6
Nathaniel Redmon <52013906+nonstopper0@users.noreply.github.com>
2019-12-03 13:32:02 -0700

    game logic using jquery and css created. controls working through key up and keydown input have also been created

--- js/app.js
+++ js/app.js
@@ -1,9 +1,54 @@
-gameCanvas = $('.game')
-
-
-
-
-
 $(() => {
-    console.log(gameCanvas);
-})
+
+
+let car = $('#car')
+let gameWindow = $('#game')
+let gameWidth = parseInt(gameWindow.width());
+let gameHeight = parseInt(gameWindow.height());
+let speed = 5;
+let score = 0;
+let running = true;
+let right = false;
+let left = false;
+
+// key input detection using jquey
+// dont want to pickup input if the game isnt running
+if (running == true) {
+    $('body').on("keydown", (event) => {
+        let key = event.keyCode;
+        if (key == 39) {
+            right = true;
+        } else if (key == 37) {
+            left = true;
+        }
+    })
+    $('body').on('keyup', (event) => {
+        let key = event.keyCode;
+        if (key == 39) {
+            right = false;
+        } else if (key == 37) {
+            left = false;
+
+        }
+    })
+}
+
+
+
+
+
+const gameLoop = setInterval(() => {
+    enemyRandomLeft = Math.floor(Math.random()*gameWidth);
+    console.log(enemyRandomLeft);
+    // check if game is running before accepting any paramters
+    // basic controls
+    if (running == true) {
+        if (left == true && car.position().left > 70) {
+            car.css("left", parseInt(car.css('left'))-10);
+        } else if (right == true && car.position().left < gameWidth+20) {
+            car.css('left', parseInt(car.css('left'))+10);
+        }
+    }
+}, 50)
+
+});
