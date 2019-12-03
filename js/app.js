005acffcaf23cb8d3f27398d95dc126d8d63f2f3
Nathaniel Redmon <52013906+nonstopper0@users.noreply.github.com>
2019-12-03 16:23:49 -0700

    added images for cones and the background road. Aswell as implemented them to animate accordingly, other small bug fixes

--- js/app.js
+++ js/app.js
@@ -44,12 +44,17 @@ const collisionDetection = () => {
     carPosition = car.offset()
     enemy1Position = enemy1.offset()
     enemy2Position = enemy2.offset();
-    if (enemy1Position.top > 340) {
-        if (enemy1Position.left > (carPosition.left-70) && enemy1Position.left < (carPosition.left + 70)) {
-            console.log("colliding");
+    if (enemy1Position.top > 370 && enemy1Position.top < 440) {
+        if (enemy1Position.left > (carPosition.left-50) && enemy1Position.left < (carPosition.left + 60)) {
+            running = false;
+        } else if (enemy2Position.top > 370 && enemy2Position.top < 440) {
+            if (enemy2Position.left > (carPosition.left-50) && enemy2Position.left < (carPosition.left + 60)) {
+                running = false;
+            }
+        }
     }
 }
-}
+
 
 $(() => {
 
@@ -81,17 +86,29 @@ const gameLoop = setInterval(() => {
     // check if game is running before accepting any paramters
     // basic controls
     if (running == true) {
+
         if (left == true && car.position().left > -50) {
             car.css("left", parseInt(car.css('left'))-speed);
         } else if (right == true && car.position().left < gameWidth-50) {
             car.css('left', parseInt(car.css('left'))+speed);
         }
+
+        if (score == 30) {
+            enemySpeed = 7;
+            gameWindow.css('animation', "animatedBackground 350s linear infinite")
+        } else if (score == 60) {
+            enemySpeed = 8; 
+            speed = 8;
+            gameWindow.css('animation', "animatedBackground 300s linear infinite");
+        }
+        
+        enemyLogic();
+        collisionDetection();
+        if (running == false) {
+            $('#game').append('<h2>You have LOST</h2>')
+            gameWindow.css('animation', 'none');
+        }
     }
     // change the cars speed based upon score 
-    if (score == 60) {
-        speed = +10;
-    }
-    enemyLogic();
-    collisionDetection();
-}, 1000/40)
+}, 1000/60)
 });
