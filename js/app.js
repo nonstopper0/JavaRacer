2018c4049d964ade94f07466c6c3599aa2a09807
Nathaniel Redmon <52013906+nonstopper0@users.noreply.github.com>
2019-12-09 13:30:48 -0700

    fixed enemy logic

--- js/app.js
+++ js/app.js
@@ -63,21 +63,25 @@ const collisionDetection = () => {
     enemy1Position = enemy1.offset();
     enemy2Position = enemy2.offset();
     enemy3Position = enemy3.offset();
+    console.log(enemy3Position);
     // these if statements only run if the enemy is level with the player, preventing collision being detected when the cone is not near the play on the y axis
     if (enemy1Position.top > 380 && enemy1Position.top < 440) {
         if (enemy1Position.left > (carPosition.left-50) && enemy1Position.left < (carPosition.left + 60)) {
             running = false;
         } 
-    else if (enemy2Position.top > 380 && enemy2Position.top < 440) {
+    }
+    if (enemy2Position.top > 380 && enemy2Position.top < 440) {
         if (enemy2Position.left > (carPosition.left-50) && enemy2Position.left < (carPosition.left + 60)) {
             running = false;
         }
-    else if (enemy3Position.top > 380 && enemy3Position.top < 440) {
+    }
+    if (enemy3Position.top > 380 && enemy3Position.top < 440) {
+
         if (enemy3Position.left > (carPosition.left-50) && enemy3Position.left < (carPosition.left + 60)) {
             running = false;
         }
     }
-}}}
+}
 
 // game speed modifier called from up and down button key pushes
 const gameSpeed = (modifier) => {
